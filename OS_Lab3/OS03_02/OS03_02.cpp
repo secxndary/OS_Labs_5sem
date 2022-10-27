2777e8e5ad5c5669cb37a0b45e69963633ac2243
secxndary <valdaitsevv@mail.ru>
2022-10-27 19:05:43 +0300

    Lab 3 — tasks 1-4

---
+++ OS_Lab3/OS03_02/OS03_02.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	cout << "====================  OS03_02  ====================\n\n";
+
+	LPCWSTR OS03_02_1 = L"C:\\Users\\valda\\source\\repos\\semester#5\\ОСИ\\OS_Lab3\\x64\\Debug\\OS03_02_1.exe";
+	LPCWSTR OS03_02_2 = L"C:\\Users\\valda\\source\\repos\\semester#5\\ОСИ\\OS_Lab3\\x64\\Debug\\OS03_02_2.exe";
+	STARTUPINFO si;	
+	PROCESS_INFORMATION pi;	
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+
+
+	if (CreateProcess(OS03_02_1, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+		cout << "[OK] Process OS03_02_1 was created.\n";
+	else cout << "[ERROR] Process OS03_02_1 was not created.\n";
+
+
+	if (CreateProcess(OS03_02_2, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+		cout << "[OK] Process OS03_02_2 was created.\n\n";
+	else cout << "[ERROR] Process OS03_02_2 was not created.\n\n";
+
+
+	for (short i = 1; i <= 100; ++i)
+	{
+		cout << i << ". PID = " << GetCurrentProcessId() << "\n";
+		Sleep(1000);
+	}
+
+
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+	return 0;
+}
+
+// CMD: tasklist /FI "IMAGENAME eq OS03_02*"
---
+++ OS_Lab3/OS03_03/OS03_03.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "TlHelp32.h"
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	DWORD pid = GetCurrentProcessId();
+	HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+	PROCESSENTRY32 peProcessEntry;
+	peProcessEntry.dwSize = sizeof(PROCESSENTRY32);
+	wcout << L"Current PID: " << pid << endl;
+
+	try
+	{
+		if (!Process32First(snap, &peProcessEntry))
+			throw L"Process32First";
+		do
+		{
+			if (peProcessEntry.th32ProcessID == pid)
+				wcout << "  ! CURRENTLY RUNNING PROCESS !\n";
+			wcout << L"Name\t\t" << peProcessEntry.szExeFile << endl << L"PID\t\t" << peProcessEntry.th32ProcessID;
+			wcout << L"\nParent PID\t" << peProcessEntry.th32ParentProcessID << "\n";
+			wcout << L"-----------------------------------\n";
+
+		} while (Process32Next(snap, &peProcessEntry));
+	}
+	catch (char* errMessage)
+	{
+		wcout << L"[ERROR] " << errMessage;
+	}
+
+	system("pause");
+	return 0;
+}
