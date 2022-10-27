092b0c175a2f573fda3523ea37d731c4adb7655b
secxndary <valdaitsevv@mail.ru>
2022-10-27 22:39:14 +0300

    Lab 3 v1.0.1 (added comments in OS03_02)

--- OS_Lab3/OS03_02/OS03_02.cpp
+++ OS_Lab3/OS03_02/OS03_02.cpp
@@ -8,18 +8,32 @@ int main()
 
 	LPCWSTR OS03_02_1 = L"C:\\Users\\valda\\source\\repos\\semester#5\\ОСИ\\OS_Lab3\\x64\\Debug\\OS03_02_1.exe";
 	LPCWSTR OS03_02_2 = L"C:\\Users\\valda\\source\\repos\\semester#5\\ОСИ\\OS_Lab3\\x64\\Debug\\OS03_02_2.exe";
-	STARTUPINFO si;	
-	PROCESS_INFORMATION pi;	
-	ZeroMemory(&si, sizeof(STARTUPINFO));
-	si.cb = sizeof(STARTUPINFO);
+	STARTUPINFO si;									// структура для определения внешнего вида окна для нового процесса	(ширина и высота окна, его смещение, заголовок)
+	PROCESS_INFORMATION pi1;						// структура с инфой о созданном процессе и его первичном потоке
+	PROCESS_INFORMATION pi2;
+	ZeroMemory(&si, sizeof(STARTUPINFO));			// обнуление памяти STARTUPINFO
+	si.cb = sizeof(STARTUPINFO);					// явное указание размера структуры si в байтах
+	si.dwFlags = STARTF_USEFILLATTRIBUTE;			// флаг для возможности работы dwFillAttribute
+	si.dwFillAttribute = (DWORD)FOREGROUND_GREEN;	// параметр цвета текста
+	si.lpReserved = NULL;							// lpReserved рекомендуется установить в null перед передачей si в CreateProcess
 
 
-	if (CreateProcess(OS03_02_1, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+	if (CreateProcess(	
+		OS03_02_1,			// lpApplicationName:	имя исполняемого модуля - exe иди другой тип (MS-DOS или OS/2)
+		NULL,				// lpCommandLine:		параметры коммандной строки
+		NULL,				// lpProcessAttributes: может ли возвращаемый дескриптор быть унаследован дочерними процессами (null - нельзя наследовать)
+		NULL,				// lpThreadAttributes:	может ли возвращаемый дескриптор быть унаследован дочерними потоками (null - нельзя наследовать)
+		FALSE,				// bInheritHandles:		true - каждый возвращаемый дескриптор наследуется дочерним процессом; false - не наследуется
+		CREATE_NEW_CONSOLE, // dwCreationFlags:		флаги, управляющие приоритетом и параметрами процесса; конкретно этот создает новый инстанс консоли
+		NULL,				// lpEnvironment:		блок конфигурации нового процесса (пары ключ-значение); если null, то конфигурация наследуется от родителя
+		NULL,				// lpCurrentDirectory:	полный путь дочернего процесса; если null, то процесс создается в каталоге родительского процесса
+		&si,				// lpStartupInfo:		структура STARTUPINFO (внешний вид окна)
+		&pi1))				// lpProcessInfo:		структура PROCESS_INFORMATION (дескрипторы процесса и первичного потока)
 		cout << "[OK] Process OS03_02_1 was created.\n";
 	else cout << "[ERROR] Process OS03_02_1 was not created.\n";
 
 
-	if (CreateProcess(OS03_02_2, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+	if (CreateProcess(OS03_02_2, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi2))
 		cout << "[OK] Process OS03_02_2 was created.\n\n";
 	else cout << "[ERROR] Process OS03_02_2 was not created.\n\n";
 
@@ -31,10 +45,14 @@ int main()
 	}
 
 
-	WaitForSingleObject(pi.hProcess, INFINITE);
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);
+	WaitForSingleObject(pi1.hProcess, INFINITE);	// бесконечно будет ожидаться завершение программы до момента 
+	WaitForSingleObject(pi2.hProcess, INFINITE);	// пока оба дочерних процесса не пошлют сигнал о завершении работы
+	CloseHandle(pi1.hThread);	// CloseHandle закрывает дескрипторы в структуре PROCESS_INFORMATION
+	CloseHandle(pi2.hThread);		
+	CloseHandle(pi1.hProcess);	// Явно закрываются первичный поток и процесс
+	CloseHandle(pi2.hProcess);
 	return 0;
 }
+
 
 // CMD: tasklist /FI "IMAGENAME eq OS03_02*"
--- OS_Lab3/OS03_05.c
+++ OS_Lab3/OS03_05.c
@@ -8,24 +8,28 @@
 
 void cycle(int counter, char* message)
 {
-	for (int i = 1; i <= counter; ++i)
-	{
-		printf("%d. PID = %d [%s]\n", i, getpid(), message);
-		sleep(2);
-	}
+    for (int i = 1; i <= counter; ++i)
+    {
+        printf("%d. PID = %d [%s]\n", i, getpid(), message);
+        sleep(2);
+    }
 }
 
 
 int main()
 {
-	pid_t pid;
-	switch (pid = fork())
-	{
-	case -1: perror("[ERROR] Fork() returned -1"); exit(-1);
-	case  0: cycle(50, "OS03_05-1"); exit(0);
-	default: cycle(100, "OS03_05"); wait(NULL);
-	}
-	exit(0);
+    pid_t pid;
+    switch (pid = fork())
+    {
+    case -1: perror("[ERROR] Fork() returned -1");
+             exit(-1);
+    case  0: cycle(50, "OS03_05-1");
+             exit(0);
+    default: printf("[INFO] default = %d\n", pid);
+             cycle(100, "OS03_05");
+             wait(NULL);
+    }
+    exit(0);
 }
 
 // Найти форкнутые процессы: ps -A | grep .out
