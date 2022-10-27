f9deff234c155994c497b0db559a54f5653c7516
secxndary <valdaitsevv@mail.ru>
2022-10-27 21:27:58 +0300

    Lab 3 v1.0

---
+++ OS_Lab3/OS03_07.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main()
+{
+    char* const argv[] = { "ps", "-ef", 0 };
+    execv("./05_1.out", argv);
+    for (int i = 1; i <= 100; ++i)
+    {
+        printf("%d. PID = %d [OS03_07]\n", i, getpid());
+        sleep(1);
+    }
+    exit(0);
+}
