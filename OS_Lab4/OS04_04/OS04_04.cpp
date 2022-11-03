fa55e19cc6aedd196e21d7238f27651c52ec833c
secxndary <valdaitsevv@mail.ru>
2022-11-03 17:46:13 +0300

    Lab 3 task 5

--- OS_Lab4/OS04_04/OS04_04.cpp
+++ OS_Lab4/OS04_04/OS04_04.cpp
@@ -47,8 +47,8 @@ int main()
 	DWORD parentId = GetCurrentThreadId();
 	DWORD childId_T1 = NULL;
 	DWORD childId_T2 = NULL;
-	HANDLE handleClild_T1 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ChildThread_T1, NULL, 0, &childId_T1);
-	HANDLE handleClild_T2 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ChildThread_T2, NULL, 0, &childId_T2);
+	HANDLE handleClild_T1 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ChildThread_T1, NULL, NULL, &childId_T1);
+	HANDLE handleClild_T2 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ChildThread_T2, NULL, NULL, &childId_T2);
 
 
 	for (short i = 1; i <= 100; ++i)
