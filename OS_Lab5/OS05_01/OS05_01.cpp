d2bd002a033d32a69a2163aeec26b695d56f27d4
secxndary <valdaitsevv@mail.ru>
2022-11-17 22:07:37 +0300

    Lab 5 v0.2

--- OS_Lab5/OS05_01/OS05_01.cpp
+++ OS_Lab5/OS05_01/OS05_01.cpp
@@ -1,7 +1,73 @@
 ﻿#include <iostream>
 #include <cstdlib>
+#include <intrin.h>
+#include <iomanip>
 #include "Windows.h"
 using namespace std;
+
+
+
+void PrintProcessPriority(HANDLE processHandle)
+{
+	DWORD processPriority = GetPriorityClass(processHandle);
+
+	switch (processPriority)
+	{
+	case IDLE_PRIORITY_CLASS:				cout << "Process priority:  IDLE_PRIORITY_CLASS\n";			break;
+	case BELOW_NORMAL_PRIORITY_CLASS:		cout << "Process priority:  BELOW_NORMAL_PRIORITY_CLASS\n";	break;
+	case NORMAL_PRIORITY_CLASS:				cout << "Process priority:  NORMAL_PRIORITY_CLASS\n";		break;
+	case ABOVE_NORMAL_PRIORITY_CLASS:		cout << "Process priority:  ABOVE_NORMAL_PRIORITY_CLASS\n";	break;
+	case HIGH_PRIORITY_CLASS:				cout << "Process priority:  HIGH_PRIORITY_CLASS\n";			break;
+	case REALTIME_PRIORITY_CLASS:			cout << "Process priority:  REALTIME_PRIORITY_CLASS\n";		break;
+	default:								cout << "[ERROR] Unknown process priority.\n";				break;
+	}
+}
+
+
+
+
+void PrintThreadPriority(HANDLE threadHandle)
+{
+	DWORD threadPriority = GetThreadPriority(threadHandle);
+
+	switch (threadPriority)
+	{
+		case THREAD_PRIORITY_LOWEST:		cout << "Thread proiority:  THREAD_PRIORITY_LOWEST\n";		break;
+		case THREAD_PRIORITY_BELOW_NORMAL:	cout << "Thread proiority:  THREAD_PRIORITY_BELOW_NORMAL\n";	break;
+		case THREAD_PRIORITY_NORMAL:		cout << "Thread proiority:  THREAD_PRIORITY_NORMAL\n";		break;
+		case THREAD_PRIORITY_ABOVE_NORMAL:	cout << "Thread proiority:  THREAD_PRIORITY_ABOVE_NORMAL\n";	break;
+		case THREAD_PRIORITY_HIGHEST:		cout << "Thread proiority:  THREAD_PRIORITY_HIGHEST\n";		break;
+		case THREAD_PRIORITY_IDLE:			cout << "Thread proiority:  THREAD_PRIORITY_IDLE\n";			break;
+		default:							cout << "[ERROR] Unknown thread priority.\n";				break;
+	}
+}
+
+
+
+
+void PrintAffinityMask(HANDLE hp, HANDLE ht) {
+	DWORD_PTR pa = NULL, sa = NULL, icpu = -1;
+	char buf[10];
+
+	if (!GetProcessAffinityMask(hp, &pa, &sa))
+		throw "[FATAL] GetProcessAffinityMask threw an exception.";
+
+	_itoa_s(pa, buf, 2);
+	cout << "Process affinity mask: " << buf;
+	cout << " (" << showbase << hex << pa << ")\n";
+	_itoa_s(sa, buf, 2);
+	cout << "System affinity mask:  " << buf;
+	cout << " (" << showbase << hex << sa << ")\n";
+
+
+	SYSTEM_INFO sys_info;
+	GetSystemInfo(&sys_info);
+	cout << "Max processors count:  " << dec << sys_info.dwNumberOfProcessors << "\n";
+	icpu = SetThreadIdealProcessor(ht, MAXIMUM_PROCESSORS);
+	cout << "Thread IdealProcessor: " << dec << icpu << "\n";
+}
+
+
 
 
 int main()
@@ -10,24 +76,17 @@ int main()
 	HANDLE threadHandle = GetCurrentThread();
 	DWORD pid = GetCurrentProcessId();
 	DWORD tid = GetCurrentThreadId();
-	DWORD processPriority = GetPriorityClass(processHandle);
+	
 
-	cout << "------------------------------------------\n\n";
+
+	cout << "--------------------------------------------\n\n";
 	cout << "Current PID:       " << pid << "\n";
-
-	switch (processPriority) 
-	{
-	case IDLE_PRIORITY_CLASS:			cout << "Process Priority:  IDLE_PRIORITY_CLASS\n";			break;
-	case BELOW_NORMAL_PRIORITY_CLASS:	cout << "Process Priority:  BELOW_NORMAL_PRIORITY_CLASS\n";	break;
-	case NORMAL_PRIORITY_CLASS:			cout << "Process Priority:  NORMAL_PRIORITY_CLASS\n";		break;
-	case ABOVE_NORMAL_PRIORITY_CLASS:	cout << "Process Priority:  ABOVE_NORMAL_PRIORITY_CLASS\n";	break;
-	case HIGH_PRIORITY_CLASS:			cout << "Process Priority:  HIGH_PRIORITY_CLASS\n";			break;
-	case REALTIME_PRIORITY_CLASS:		cout << "Process Priority:  REALTIME_PRIORITY_CLASS\n";		break;
-	default:							cout << "[ERROR] Unknown process priority.\n";				break;
-	}
-
-
-	cout << "\n------------------------------------------\n\n";
+	PrintProcessPriority(processHandle);
+	cout << "Current TID:       " << tid << "\n";
+	PrintThreadPriority(threadHandle);
+	cout << "\n";
+	PrintAffinityMask(processHandle, threadHandle);
+	cout << "\n--------------------------------------------\n\n";
 
 	system("pause");
 }
