eea03ee4a20439230a8c8aef232743fdbced88b8
secxndary <valdaitsevv@mail.ru>
2022-11-17 21:07:44 +0300

    Lab 3 v0.1

---
+++ OS_Lab5/OS05_01/OS05_01.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include "Windows.h"
+using namespace std;
+
+
+int main()
+{
+	HANDLE processHandle = GetCurrentProcess();
+	HANDLE threadHandle = GetCurrentThread();
+	DWORD pid = GetCurrentProcessId();
+	DWORD tid = GetCurrentThreadId();
+	DWORD processPriority = GetPriorityClass(processHandle);
+
+	cout << "------------------------------------------\n\n";
+	cout << "Current PID:       " << pid << "\n";
+
+	switch (processPriority) 
+	{
+	case IDLE_PRIORITY_CLASS:			cout << "Process Priority:  IDLE_PRIORITY_CLASS\n";			break;
+	case BELOW_NORMAL_PRIORITY_CLASS:	cout << "Process Priority:  BELOW_NORMAL_PRIORITY_CLASS\n";	break;
+	case NORMAL_PRIORITY_CLASS:			cout << "Process Priority:  NORMAL_PRIORITY_CLASS\n";		break;
+	case ABOVE_NORMAL_PRIORITY_CLASS:	cout << "Process Priority:  ABOVE_NORMAL_PRIORITY_CLASS\n";	break;
+	case HIGH_PRIORITY_CLASS:			cout << "Process Priority:  HIGH_PRIORITY_CLASS\n";			break;
+	case REALTIME_PRIORITY_CLASS:		cout << "Process Priority:  REALTIME_PRIORITY_CLASS\n";		break;
+	default:							cout << "[ERROR] Unknown process priority.\n";				break;
+	}
+
+
+	cout << "\n------------------------------------------\n\n";
+
+	system("pause");
+}
