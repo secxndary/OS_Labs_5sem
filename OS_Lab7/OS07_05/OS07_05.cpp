d72856b08d594c15fb863b2aba750d280c99e200
secxndary <valdaitsevv@mail.ru>
2022-12-30 00:08:10 +0300

    Lab 7 – 5

--- OS_Lab7/OS07_05/OS07_05.cpp
+++ OS_Lab7/OS07_05/OS07_05.cpp
@@ -1,20 +1,55 @@
-﻿// OS07_05.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
 
-#include <iostream>
+
+
+
+PROCESS_INFORMATION createProcess(LPCWSTR path)
+{
+	STARTUPINFO startupInfo;
+	PROCESS_INFORMATION processInfo;
+
+	ZeroMemory(&startupInfo, sizeof(STARTUPINFO));
+	startupInfo.cb = sizeof(STARTUPINFO);
+
+	if (CreateProcessW(path, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &startupInfo, &processInfo))
+		cout << "Process created.\n";
+	else
+		cout << "[ERROR]\n";
+
+	return processInfo;
+}
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int pid = GetCurrentProcessId();
+	const int size = 2;
+	HANDLE event;
+	HANDLE processes[size];
+
+	processes[0] = createProcess(L"C:\\Users\\valda\\source\\repos\\semester#5\\ОСИ\\OS_Lab7\\Debug\\OS07_05A.exe").hProcess;
+	processes[1] = createProcess(L"C:\\Users\\valda\\source\\repos\\semester#5\\ОСИ\\OS_Lab7\\Debug\\OS07_05B.exe").hProcess;
+
+	event = CreateEvent(NULL, FALSE, FALSE, L"OS07_05");
+
+	for (int i = 1; i <= 90; i++)
+	{
+		if (i == 50) 
+			SetEvent(event);
+
+		printf("[OS07_05]\t %d.  PID = %d\n", i, pid);
+		Sleep(100);
+	}
+
+	WaitForMultipleObjects(size, processes, TRUE, INFINITE);
+	for (int i = 0; i < size; i++)
+		CloseHandle(processes[i]);
+
+	CloseHandle(event);
+	system("pause");
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
